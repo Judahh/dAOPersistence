67c023b26b0f44581837f160a9cb4a85260d4a8b
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2020-07-22 19:49:06 -0300

    Documentação de idéia principal para arquitetura

--- source/postgresDB.ts
+++ source/postgresDB.ts
@@ -1,60 +1,67 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import {
-//   PersistenceAdapter,
-//   DatabaseInfo,
-//   PersistencePromise,
-//   // RelationValuePostgresDB,
-//   // SelectedItemValue,
-//   PersistenceInputCreate,
-//   PersistenceInputUpdate,
-//   PersistenceInputRead,
-//   PersistenceInputDelete,
-// } from 'flexiblepersistence';
-// import { Journaly } from 'journaly';
-// import { Pool } from 'pg';
-// export class PostgresDB implements PersistenceAdapter {
-//   private databaseInfo: DatabaseInfo;
-//   private pool: Pool;
+import {
+  PersistenceAdapter,
+  DatabaseInfo,
+  PersistencePromise,
+  // RelationValuePostgresDB,
+  // SelectedItemValue,
+  PersistenceInputCreate,
+  PersistenceInputUpdate,
+  PersistenceInputRead,
+  PersistenceInputDelete,
+} from 'flexiblepersistence';
+import { Journaly } from 'journaly';
+import { Pool } from 'pg';
+export class PostgresDB implements PersistenceAdapter {
+  private databaseInfo: DatabaseInfo;
+  private pool: Pool;
 
-//   constructor(databaseInfo: DatabaseInfo) {
-//     this.databaseInfo = databaseInfo;
-//     this.pool = new Pool(this.databaseInfo);
-//   }
+  constructor(databaseInfo: DatabaseInfo) {
+    this.databaseInfo = databaseInfo;
+    this.pool = new Pool(this.databaseInfo);
+  }
 
-//   public async correct(
-//     input: PersistenceInputUpdate
-//   ): Promise<PersistencePromise> {
-//     //return (await this.service('selectById', id))[0];
-//     return (await this.journaly.publish(input.scheme + '.selectAll'))[0];
-//   }
+  public async correct(
+    input: PersistenceInputUpdate
+  ): Promise<PersistencePromise> {
+    //! Envia o input para o service determinado pelo esquema e lá ele faz as
+    //! operações necessárias usando o journaly para acessar outros Services ou
+    //! DAOs.
+    //! Sempre deve-se receber informações do tipo input e o output deve ser
+    //! compatível com o input para pemitir retro-alimentação.
+    //! Atualizar o input para que utilize o melhor dos dois
+    //! (input e parametros usados no SimpleAPI).
+    //return (await this.service('selectById', id))[0];
+    return (await this.journaly.publish(input.scheme + '.selectAll', input))[0];
+  }
 
-//   public nonexistent(
-//     input: PersistenceInputDelete
-//   ): Promise<PersistencePromise> {}
+  public nonexistent(
+    input: PersistenceInputDelete
+  ): Promise<PersistencePromise> {}
 
-//   public create(input: PersistenceInputCreate): Promise<PersistencePromise> {}
-//   public update(input: PersistenceInputUpdate): Promise<PersistencePromise> {}
-//   public read(input: PersistenceInputRead): Promise<PersistencePromise> {}
-//   public delete(input: PersistenceInputDelete): Promise<PersistencePromise> {}
+  public create(input: PersistenceInputCreate): Promise<PersistencePromise> {}
+  public update(input: PersistenceInputUpdate): Promise<PersistencePromise> {}
+  public read(input: PersistenceInputRead): Promise<PersistencePromise> {}
+  public delete(input: PersistenceInputDelete): Promise<PersistencePromise> {}
 
-//   public getDatabaseInfo(): DatabaseInfo {
-//     return this.databaseInfo;
-//   }
+  public getDatabaseInfo(): DatabaseInfo {
+    return this.databaseInfo;
+  }
 
-//   public getPool(): Pool {
-//     // TODO: remove
-//     return this.pool;
-//   }
+  public getPool(): Pool {
+    // TODO: remove
+    return this.pool;
+  }
 
-//   public close(): Promise<unknown> {
-//     return new Promise<unknown>((resolve) => {
-//       this.end(resolve);
-//     });
-//   }
+  public close(): Promise<unknown> {
+    return new Promise<unknown>((resolve) => {
+      this.end(resolve);
+    });
+  }
 
-//   private end(resolve): void {
-//     this.pool.end(() => {
-//       resolve();
-//     });
-//   }
-// }
+  private end(resolve): void {
+    this.pool.end(() => {
+      resolve();
+    });
+  }
+}
