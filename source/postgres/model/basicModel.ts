ae5b76ae536b6adb93fd93be66a0193d3ad341c3
Judahh <judahholanda@gmail.com>
2021-09-28 14:41:13 -0300

    remove postgres

--- source/postgres/model/basicModel.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from 'mongodb';
-
-export abstract class BasicModel {
-  private id?: string | RegExp | ObjectId;
-
-  constructor(id: string | RegExp | ObjectId) {
-    this.id = id;
-  }
-}
