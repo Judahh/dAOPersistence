7fd37b568081c12be55d51c9e22a785082e4493d
Judahh <judahholanda7@gmail.com>
2022-02-03 14:41:04 -0300

    show

--- source/dAO/baseDAO.ts
+++ source/dAO/baseDAO.ts
@@ -151,6 +151,7 @@ export default abstract class BaseDAO extends BaseDAODefault {
   ): Promise<typeof content extends [] ? IDAO[] : IDAO> {
     // console.log('UPDATE QUERY:', query, values);
     return new Promise(async (resolve, reject) => {
+      console.log('QUERY:', query, values);
       let result = await this.pool?.query(query, values).catch(reject);
       type ResultType = typeof content extends [] ? IDAO[] : IDAO;
       result = this.fixType(result ? result : {});
