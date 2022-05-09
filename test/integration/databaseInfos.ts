3ce589cecb1b83b0aed072679637cca39cb371bc
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-05-09 12:11:00 -0300

    new try

--- test/integration/databaseInfos.ts
+++ test/integration/databaseInfos.ts
@@ -1,3 +1,5 @@
+import { Info } from 'flexiblepersistence';
+
 const eventInfo = {
   database: 'write',
   host: process.env.MONGO_HOST || 'localhost',
@@ -18,11 +20,12 @@ const readInfo = {
   },
 };
 
-const readInfo2 = {
+const readInfo2: Info = {
   database: 'read_DB',
   uri: process.env.MSSQL_URI,
   connectionType: process.env.MSSQL_CONNECTION_TYPE,
-  options: process.env.MSSQL_OPTIONS || {
+  options: (process.env.MSSQL_OPTIONS &&
+    JSON.parse(process.env.MSSQL_OPTIONS)) || {
     encrypt: false,
     trustServerCertificate: false,
   },
@@ -30,11 +33,11 @@ const readInfo2 = {
   port: process.env.MSSQL_PORT || 1433,
   username: process.env.MSSQL_USER || 'SA',
   password: process.env.MSSQL_PASSWORD || 'yourStrong(!)Password',
-  ssl: false,
+  ssl: (process.env.MSSQL_SSL && JSON.parse(process.env.MSSQL_SSL)) || false,
   pool: {
     min: 0,
     max: 10,
-    acquireTimeoutMillis: 600000,
+    // acquireTimeoutMillis: 600000,
     idleTimeoutMillis: 600000,
   },
 };
